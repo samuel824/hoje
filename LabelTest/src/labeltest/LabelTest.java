07c91a02f7bbda70233dca48bf8d611bf7bebe4b
samuel824 <60981495+samuel824@users.noreply.github.com>
2020-03-04 15:09:51 -0400

    Add files via upload

---
+++ LabelTest/src/labeltest/LabelTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labeltest;
+
+/**
+ *
+ * @author Aluno
+ */
+ import javax.swing.JFrame;
+        
+        public class LabelTest
+        {
+	    public static void main ( String[] args)
+            {
+		LabelFrame labelFrame = new LabelFrame();
+		labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+		labelFrame.setSize(260, 180);
+		labelFrame.setVisible(true);
+	    }
+	}
